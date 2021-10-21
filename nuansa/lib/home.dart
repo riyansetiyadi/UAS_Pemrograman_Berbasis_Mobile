4b98b85ea89d7cbcabaa997afc106a6a67937633
Riyansetiyadi <setiyariyan19@gmail.com>
2021-10-22 02:02:45 +0700

    20% home screen

--- nuansa/lib/home.dart
+++ nuansa/lib/home.dart
@@ -6,9 +6,57 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: SizedBox(
+          height: kToolbarHeight,
+          child: Image.asset("assets/images/nuansa.png"),
+        ),
+        toolbarHeight: kToolbarHeight,
+        actions: [
+          IconButton(
+            onPressed: () {}, 
+            icon: Icon(Icons.notifications_none)
+          )
+        ],
+      ),
       body: Center(
-        child: Center(
-          child: Text("data"),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Image.asset(
+                    "assets/images/home_bg.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                  FractionallySizedBox(
+                    widthFactor: 0.75,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(25.0),
+                      elevation: 10.0,
+                      shadowColor: Colors.blue,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: BorderSide(
+                              width: 0, 
+                              style: BorderStyle.none,
+                            ),
+                          ),
+                          prefixIcon: Icon(Icons.search),
+                          hintText: "Kemana?",
+                          fillColor: Colors.white,
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         )
       ),
     );
