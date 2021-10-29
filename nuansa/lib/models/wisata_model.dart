54f91b7228f7278993a3f2f34591b5cb1205674a
Riyansetiyadi <setiyariyan19@gmail.com>
2021-10-29 15:22:15 +0700

    model untuk list wisata

---
+++ nuansa/lib/models/wisata_model.dart
@@ -0,0 +1,13 @@
+class ModelWisata {
+  final int id;
+  final String foto, nama, lokasi, deskripsi;
+  
+  
+  ModelWisata({
+    required this.id,
+    required this.foto,
+    required this.nama,
+    required this.lokasi,
+    required this.deskripsi
+  });
+}
