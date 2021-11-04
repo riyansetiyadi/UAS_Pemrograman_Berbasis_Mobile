b40ee2d330e065ae8a057ec46748627224d32fa4
Riyansetiyadi <setiyariyan19@gmail.com>
2021-11-04 11:43:17 +0700

    update scroller

--- nuansa/lib/profil.dart
+++ nuansa/lib/profil.dart
@@ -8,51 +8,52 @@ class ProfilScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: Container(
-      margin:EdgeInsets.only(left:80.0, right:40.0, top:20.0,),
-      
-      padding: EdgeInsets.all(20.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              "My Profil",
-              style: TextStyle(
-                fontSize: 30,
+    body: SingleChildScrollView(
+      child: Container(
+        
+        padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                "My Profil",
+                style: TextStyle(
+                  fontSize: 30,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(80.0),
-              child: Image.asset(
-                'assets/images/home_bg.jpg',
-                width: 150.0,
-                height: 150.0,
-                fit: BoxFit.cover,
+              SizedBox(
+                height: 20,
               ),
-            ),
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.all(10),
-              height: 500,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3))
-                  
-                ],
+              ClipRRect(
+                borderRadius: BorderRadius.circular(80.0),
+                child: Image.asset(
+                  'assets/images/home_bg.jpg',
+                  width: 150.0,
+                  height: 150.0,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-          ],
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(10),
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3))
+                    
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+    ),
     );
   }
 }
