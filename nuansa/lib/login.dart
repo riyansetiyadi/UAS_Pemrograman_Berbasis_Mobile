d75390e14d83acf21a1045e21eec2bd80d45ca82
Riyansetiyadi <setiyariyan19@gmail.com>
2021-11-07 04:09:00 +0700

    menambah fitur autentikasi dan menghubungkan ke database

--- nuansa/lib/login.dart
+++ nuansa/lib/login.dart
@@ -2,39 +2,72 @@ import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
 import 'models/user_model.dart';
 import 'navigation.dart';
+import 'db/database.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  DbHelper dbHelper = DbHelper();
+  int count = 0;
+  UserModel? userData;
+  List<UserModel>? userList;
 
   Duration get loginTime => Duration(milliseconds: 2250);
 
-  Future<String?>? _authUser(LoginData data) {
+  Future<String?>? _onUserLogin(LoginData data) {
+    Future<List<UserModel>> userDataListFuture = dbHelper.getUserByEmail(data.name);
+    userDataListFuture.then((userDataList) {
+      userList = userDataList;
+      count = userDataList.length;
+    });
     return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(data.name)) {
-        return 'User not exists';
-      }
-      if (users[data.name] != data.password) {
-        return 'Password does not match';
+      if (count != 0) {
+        if (userList![0].email != data.name) {
+          return 'User not exists';
+        }
+        print(userList!);
+        if (userList![0].kataSandi != data.password) {
+          return 'Password does not match';
+        }
+      } else {
+        return 'Email tidak terdaftar';
       }
       return null;
     });
   }
 
+  Future<String?>? _onUserSignUp(LoginData data) {
+    userData = UserModel(null, data.name, data.name, data.password);
+    return Future.delayed(loginTime).then((_) async {
+      int result = await dbHelper.insertUser(userData!);
+      if (result > 0) {
+        return null;
+      }
+      return 'Email sudah terdaftar';
+    });
+  }
+
   Future<String?>? _recoverPassword(String name) {
     return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(name)) {
-        return 'User not exists';
-      }
+      // if (!users.containsKey(name)) {
+      //   return 'User not exists';
+      // }
       return null;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    
     return FlutterLogin(
       title: "Nuansa.co",
-      onLogin: _authUser,
-      onSignup: _authUser,
+      onLogin: _onUserLogin,
+      onSignup: _onUserSignUp,
       onSubmitAnimationCompleted: () {
         Navigator.of(context).pushReplacement(MaterialPageRoute(
           builder: (context) => Navigation(),
@@ -43,6 +76,7 @@ class LoginScreen extends StatelessWidget {
       onRecoverPassword: _recoverPassword,
       messages: LoginMessages(
         passwordHint: "Kata Sandi",
+        confirmPasswordHint: "Konfirmasi Kata Sandi",
         forgotPasswordButton: "Lupa Password?",
         goBackButton: "Kembali",
         loginButton: "Masuk",
@@ -56,5 +90,11 @@ class LoginScreen extends StatelessWidget {
         flushbarTitleSuccess: "Sukses"
       ),
     );
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
   }
 }
