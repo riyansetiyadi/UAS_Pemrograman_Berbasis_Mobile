85530eeb3c8519c2fd2d9cde55e731650566b798
Riyansetiyadi <setiyariyan19@gmail.com>
2021-11-07 04:08:03 +0700

    update model agar cocok dengan db

--- nuansa/lib/models/user_model.dart
+++ nuansa/lib/models/user_model.dart
@@ -1,4 +1,38 @@
-const users = {
-  'dribbble@gmail.com': '12345',
-  'hunter@gmail.com': 'hunter',
-};
+class UserModel{
+  int? _id = 0;
+  String _nama = "";
+  String _email = "";
+  String _kataSandi = "";
+
+  UserModel(this._id, this._nama, this._email, this._kataSandi);
+
+  UserModel.fromMap(Map<String, dynamic> map){
+    this._id=map['id'];
+    this._nama=map['nama'];
+    this._email=map['email'];
+    this._kataSandi=map['kataSandi'];
+  }
+  int? get id => _id;
+  String get nama => _nama;
+  String get email => _email;
+  String get kataSandi => _kataSandi;
+
+  set nama(String value){
+    _nama=value;
+  }
+  set email(String value) {
+    _email = value;
+  }
+  set kataSandi(String value) {
+    _kataSandi = value;
+  }
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = Map<String, dynamic>();
+    map['id'] = this._id;
+    map['nama'] = nama;
+    map['email'] = email;
+    map['kataSandi'] = kataSandi;
+    return map;
+  } 
+}
--- nuansa/lib/models/wisata_model.dart
+++ nuansa/lib/models/wisata_model.dart
@@ -1,13 +1,52 @@
-class ModelWisata {
-  final int id;
-  final String foto, nama, lokasi, deskripsi;
-  
-  
-  ModelWisata({
-    required this.id,
-    required this.foto,
-    required this.nama,
-    required this.lokasi,
-    required this.deskripsi
-  });
+class WisataModel{
+  int? _id;
+  String _foto = "";
+  String _nama = "";
+  String _kota = "";
+  String _negara = "";
+  String _deskripsi = "";
+
+  WisataModel(this._id, this._foto, this._nama, this._kota, this._negara, this._deskripsi);
+
+  WisataModel.fromMap(Map<String, dynamic> map){
+    this._id=map['id'];
+    this._foto=map['foto'];
+    this._nama=map['nama'] ?? "";
+    this._kota=map['kota'] ?? "";
+    this._negara=map['negara'] ?? "";
+    this._deskripsi=map['deskripsi'] ?? "";
+  }
+  int? get id => _id;
+  String get foto => _foto;
+  String get nama => _nama;
+  String get kota => _kota;
+  String get negara => _negara;
+  String get deskripsi => _deskripsi;
+
+  set foto(String value){
+    _foto=value;
+  }
+  set nama(String value){
+    _nama=value;
+  }
+  set kota(String value) {
+    _kota = value;
+  }
+  set negara(String value) {
+    _negara = value;
+  }
+  set deskripsi(String value) {
+    _deskripsi = value;
+  }
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = Map<String, dynamic>();
+    map['id'] = this._id;
+    map['foto'] = foto;
+    map['nama'] = nama;
+    map['kota'] = kota;
+    map['negara'] = negara;
+    map['deskripsi'] = deskripsi;
+    return map;
+  } 
 }
